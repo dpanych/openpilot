3770b5102292f600c4fdaff28d39de6786b255a1
David Panych <58536732+dpanych@users.noreply.github.com>
2021-05-07 10:44:36 -0700

    Update carcontroller.py

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -1,70 +1,60 @@
-from selfdrive.car import apply_toyota_steer_torque_limits
+from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.chrysler.chryslercan import create_lkas_hud, create_lkas_command, \
-                                               create_wheel_buttons
+  create_wheel_buttons
 from selfdrive.car.chrysler.values import CAR, CarControllerParams
 from opendbc.can.packer import CANPacker
+
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.apply_steer_last = 0
-    self.ccframe = 0
-    self.prev_frame = -1
+    self.braking = False
+    self.alert_active = False
     self.hud_count = 0
     self.car_fingerprint = CP.carFingerprint
-    self.gone_fast_yet = False
     self.steer_rate_limited = False
+    self.prev_frame = -1
+    self.steer_command_bit = False
 
     self.packer = CANPacker(dbc_name)
 
-  def update(self, enabled, CS, actuators, pcm_cancel_cmd, hud_alert):
-    # this seems needed to avoid steering faults and to force the sync with the EPS counter
-    frame = CS.lkas_counter
-    if self.prev_frame == frame:
-      return []
+  def update(self, enabled, CS, frame, actuators, pcm_cancel_cmd, leftLaneVisible,
+             rightLaneVisible):  # TODO hud_alert
+    P = CarControllerParams
+    
+    steer_ready = CS.out.vEgo > CS.CP.minSteerSpeed
+    
+    if steer_ready:
+      self.steer_command_bit = True
+    if not steer_ready:
+      self.steer_command_bit = False
+    
+    bad_to_bone = enabled and steer_ready
 
-    # *** compute control surfaces ***
-    # steer torque
-    new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
-    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last,
-                                                   CS.out.steeringTorqueEps, CarControllerParams)
+    if bad_to_bone:
+      new_steer = int(round(actuators.steer * P.STEER_MAX))
+    else:
+      new_steer = 0
+
+    apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
     self.steer_rate_limited = new_steer != apply_steer
-
-    moving_fast = CS.out.vEgo > CS.CP.minSteerSpeed  # for status message
-    if CS.out.vEgo > (CS.CP.minSteerSpeed - 0.5):  # for command high bit
-      self.gone_fast_yet = True
-    elif self.car_fingerprint in (CAR.PACIFICA_2019_HYBRID, CAR.PACIFICA_2020, CAR.JEEP_CHEROKEE_2019):
-      if CS.out.vEgo < (CS.CP.minSteerSpeed - 3.0):
-        self.gone_fast_yet = False  # < 14.5m/s stock turns off this bit, but fine down to 13.5
-    lkas_active = moving_fast and enabled
-
-    if not lkas_active:
-      apply_steer = 0
-
     self.apply_steer_last = apply_steer
+
+    # *** control msgs ***
 
     can_sends = []
 
-    #*** control msgs ***
+    # *** control msgs ***
 
-    if pcm_cancel_cmd:
-      # TODO: would be better to start from frame_2b3
-      new_msg = create_wheel_buttons(self.packer, self.ccframe, cancel=True)
-      can_sends.append(new_msg)
+    #    if pcm_cancel_cmd:  # TODO: ENABLE ONCE STEERING WORKS
+    #      new_msg = create_wheel_buttons(self.packer, self.frame, cancel=True)
+    #      can_sends.append(new_msg)
+    counter = (frame / 2) % 16
+    
+    if frame % 2 == 0:
+      can_sends.append(create_lkas_command(self.packer, int(apply_steer), counter, self.steer_command_bit))
 
-    # LKAS_HEARTBIT is forwarded by Panda so no need to send it here.
-    # frame is 100Hz (0.01s period)
-    if (self.ccframe % 25 == 0):  # 0.25s period
-      if (CS.lkas_car_model != -1):
-        new_msg = create_lkas_hud(
-            self.packer, CS.out.gearShifter, lkas_active, hud_alert,
-            self.hud_count, CS.lkas_car_model)
-        can_sends.append(new_msg)
-        self.hud_count += 1
-
-    new_msg = create_lkas_command(self.packer, int(apply_steer), self.gone_fast_yet, frame)
-    can_sends.append(new_msg)
-
-    self.ccframe += 1
-    self.prev_frame = frame
-
+    if frame % 5 == 0:
+      can_sends.append(create_lkas_hud(self.packer, enabled, leftLaneVisible, rightLaneVisible))
+    
     return can_sends
